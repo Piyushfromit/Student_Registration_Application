9b95588ec40782bcc3113c8cb266b6911f2bb091
piyushfromit <piyushfrommasai@gmail.com>
2022-11-11 11:51:21 +0530

    day-2

---
+++ src/student/registration/util/DBUtil.java
@@ -0,0 +1,33 @@
+package student.registration.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+	public static Connection establishConnection() {
+		
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url= "jdbc:mysql://localhost:3306/studentdata";
+		
+		try {
+			conn = DriverManager.getConnection(url, "root", "mysql");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return conn;
+	}
+
+}
